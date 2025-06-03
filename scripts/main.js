32f7a4255c55df89379f0c81006ddc9cee72ef64
faruq <your.email@example.com>
2025-06-03 16:08:36 +0100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+//Store a reference to the <h1> in a variable.
+const myImage = document.querySelector('img');
+myImage.addEventListener('click', () => {
+    const mySrc = myImage.getAttribute('src');
+    if(mySrc === "images/Mozilla_Firefox_3.5_logo.png") {
+        myImage.setAttribute('src', 'images/calculator-791833_512.jpg');
+        myImage.setAttribute('alt', 'Calculator Image');
+    }   else {
+        myImage.setAttribute('src', 'images/Mozilla_Firefox_3.5_logo.png');
+    } });  
+    
+   //adding a personalized message to the heading, store the users name in the browser's localstorage API, and change user when a button is clicked.// myImage.addEventListener('mouseover', () => {
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
