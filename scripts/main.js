10ee90b68151458c8e2d2732774ae208e2b33c70
faruq <your.email@example.com>
2025-06-04 13:19:06 +0100

    keep local changes to scripts/main.js before merge

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
 //Store a reference to the <h1> in a variable.
-const myImage = document.querySelector('img');
-myImage.addEventListener('click', () => {
-    const mySrc = myImage.getAttribute('src');
-    if(mySrc === "images/Mozilla_Firefox_3.5_logo.png") {
-        myImage.setAttribute('src', 'images/calculator-791833_512.jpg');
-        myImage.setAttribute('alt', 'Calculator Image');
-    }   else {
-        myImage.setAttribute('src', 'images/Mozilla_Firefox_3.5_logo.png');
-    } });  
-    
-   //adding a personalized message to the heading, store the users name in the browser's localstorage API, and change user when a button is clicked.// myImage.addEventListener('mouseover', () => {
+const myImage = document.querySelector("img");
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Mozilla_Firefox_3.5_logo.png") {
+    myImage.setAttribute("src", "images/calculator-791833_512.jpg");
+    myImage.setAttribute("alt", "Calculator Image");
+  } else {
+    myImage.setAttribute("src", "images/Mozilla_Firefox_3.5_logo.png");
+  }
+});
+
+//adding a personalized message to the heading, store the users name in the browser's localstorage API, and change user when a button is clicked.// myImage.addEventListener('mouseover', () => {
 
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
@@ -26,7 +27,6 @@ if (!localStorage.getItem("name")) {
   const storedName = localStorage.getItem("name");
   myHeading.textContent = `Mozilla is cool, ${storedName}`;
 }
-
 
 myButton.addEventListener("click", () => {
   setUserName();
